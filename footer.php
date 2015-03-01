c23f86897ac4827d32a119defba643b0a2c7f0fb
blontz <blontz@nautilus-ins.com>
2015-02-28 17:02:44 -0700

    Abstracted database functions
    Abstracted the database functions into the mysql_crud.php class file.

---
+++ footer.php
@@ -0,0 +1,3 @@
+<?php
+echo "<p>MyCellar &copy; 2015-" . date("Y") . " Bradley Lontz</p>";
+?>
---
+++ menu.php
@@ -0,0 +1,9 @@
+<?php
+echo '<div class="menu">
+<a href="index.html">Home</a> - 
+<a href="cellar.html">Display Cellar</a> - 
+<a href="wines.html">Maintain Cellar</a> -
+<a href="varietals.html">Maintain Varietals</a> -
+<a href="about.html">About MyCellar</a>
+</div>';
+?>
