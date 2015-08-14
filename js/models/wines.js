c9f98b5b06307a155b05cbd944b486b26904262a
B Lontz <blontz@nautilus-ins.com>
2015-08-14 15:40:32 -0700

    Completed Varietal Maintenance
    Fixed up the varietal maintenance form and the models.

--- js/models/wines.js
+++ js/models/wines.js
@@ -1,25 +1,53 @@
-﻿function Wine(name, varietal) {
+﻿// Class constructor for the Wine class
+function Wine(name, varietal) {
     var self = this;
     self.name = name || "New Wine";
 
-    if (varietal === undefined || validVarieties.varietes.indexOf(varietal) === -1) {
-        varietal = validVarieties.varietes[0];
+    if (varietal === undefined || validVarietals.indexOf(varietal) === -1) {
+        console.log("Invalid varietal provided - " + varietal);
+        varietal = validVarietals[0];
     }
     self.varietal = varietal;
+}
+
+// Class constructor for the Varietal class.  This will be implemented at a
+// later point.  Initially, the varietal is being treated as a single deminsion.
+function Varietal (grape, color) {
+    var self = this;
+    
+    self.grape = grape || "Unknown";
+    self.color = color || "Red";
 }
 
 function VarietalList() {
     var self = this;
  
-    // Editable properties
-    self.varietes = ["Unknown", "Merlot", "Cab", "Pinot Nior", "Savanoc Blanc"];
+    // Load varietals into the array
+    self.varietals = ko.observableArray();
+    
+    for (i=0; i < validVarietals.length; i++) {
+        self.varietals.push(new Varietal(validVarietals[i], "Red"));
+    }
 
     // Methods
-    // Create a method to add a variety to the list
-    self.addVariety = function (varietal) {
-        var varietal = varietal || "Unknown";
-        self.varietes.push(varietal);
+    // Create a method to add a varietal to the list
+    self.addVarietal = function (varietal) {
+        self.varietals.push(new Varietal(varietal, "Red"));
+    }
+    
+    // Create a method to remove a varietal from the list
+    self.removeVarietal = function (varietal) {
+        self.varietals.remove(varietal);
+    }
+    
+    // Create a method to save the varietals list
+    self.saveVarietals = function () {
+        console.log("Saving " + self.varietals().length + " varietals.");
+        for (i=0; i < self.varietals().length; i++) {
+            console.log(self.varietals()[i]);
+        }
     }
 }
 
-var validVarieties = new VarietalList();
+// Temporary list of valid varietals until the save and load are complete
+var validVarietals = ["Unknown", "Merlot", "Cab", "Pinot Nior", "Savanoc Blanc"];
--- js/varietals_vm.js
+++ js/varietals_vm.js
@@ -1,22 +1,6 @@
-﻿function Varietal(name) {
-    var self = this;
-    self.name = name;
+﻿function initVarietalListVM () {
+    // bind the wineList instance to the KnockOut elements
+    ko.applyBindings(new VarietalList());    
 }
 
-function VarietalListViewModel(parms) {
-    var self = this;
-    
-    self.name = ko.observable('Merlot');
-    self.id = ko.observable(parms.id);
-    
-    //Operations
-    self.addVarietal = function () {
-        self.varietals.push(new Varietal("New"));
-    }
-
-    self.removeVarietal = function (varietal) { 
-        self.varietals.remove(Varietal); 
-    }
-}
-
-ko.applyBindings(new VarietalListViewModel(data));
+window.addEventListener("load", initVarietalListVM, false);
